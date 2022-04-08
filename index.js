0918e490671d260798831a9b2ec07b50cda3b17e
Sean Sarokas <sps4462@gmail.com>
2022-04-08 08:57:24 -0400

    commit tree 6d08216a30b5438d0f5964708f73e169b5779c92

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+/*function distanceFromHqInBlocks(pickUpBlock){
+    let blockTotal;
+    (pickUpBlock >= 42) ? (blockTotal = pickUpBlock - 42) : (blockTotal = 42 - pickUpBlock);
+    return blockTotal;
+}*/
+
+function distanceFromHqInBlocks(pickUpBlock){
+    return (pickUpBlock >= 42) ? (pickUpBlock - 42) : (42 - pickUpBlock);
+}
+
+function distanceFromHqInFeet(pickUpBlock){
+    return distanceFromHqInBlocks(pickUpBlock) * 264;
+}
+
+function distanceTravelledInFeet(pickUpBlock, dropOffBlock){
+    return Math.abs(pickUpBlock - dropOffBlock) * 264;
+}
+
+function calculatesFarePrice(pickUpBlock, dropOffBlock){
+    const feetDistance = Math.abs(pickUpBlock - dropOffBlock) * 264;
+    if (feetDistance <= 400) {
+        return 0;
+    }
+    else if (feetDistance <= 2000) {
+        return (feetDistance - 400) * .02;
+    }
+    else if (feetDistance <=2500){
+        return 25;
+    }
+    else if (feetDistance >2500){
+        return "cannot travel that far";
+    }
+}
